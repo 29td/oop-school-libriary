2d448a62de6467cff79213cea35dffd3e195674a
29td <nonhlanhlamndebele9@gmail.com>
2022-10-06 10:43:25 +0200

    update person.rb file

--- Person.rb
+++ Person.rb
@@ -16,11 +16,10 @@ class Person < Nameable
     of_age? >= 18 || @parent_permission
   end
 
-  def add_rental(rental)
-    @rentals.push(rental)
-    rental.person = self
-  end
-
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+ end
+ 
   def correct_name
     @name
   end
