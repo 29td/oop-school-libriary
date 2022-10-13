dfb7fa11231bcf2ef7f2eb8c10a82bf9fbee9604
29td <nonhlanhlamndebele9@gmail.com>
2022-10-13 12:49:52 +0200

    fix linter errors

--- spec/capdecorate_spec.rb
+++ spec/capdecorate_spec.rb
@@ -1,11 +1,11 @@
 require_relative 'spec_helper'
 
 describe Capdecorate do
-    context 'With valid arguments' do
-      it 'should capitalize name of a person' do
-        new_person = Person.new(34, nil, 'nicHolas')
-        cap_name = Capdecorate.new(new_person)
-        expect(cap_name.correct_name).to eq 'Nicholas'
-      end
+  context 'With valid arguments' do
+    it 'should capitalize name of a person' do
+      new_person = Person.new(34, nil, 'nicHolas')
+      cap_name = Capdecorate.new(new_person)
+      expect(cap_name.correct_name).to eq 'Nicholas'
     end
   end
+end
